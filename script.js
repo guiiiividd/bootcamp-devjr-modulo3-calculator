3d66b14a5ea248da2f276dda9511e44b2adc6dde
guiiiividd <guiiiividd@gmail.com>
2023-10-11 15:59:59 -0300

    closes #14 - Implementando as 4 Operações Matemáticas

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function calcular(){
+function calcular(op){
     let num1 = parseFloat(document.getElementById("num1").value)
     let num2 = parseFloat(document.getElementById("num2").value)
 
-    let output = document.getElementById("output")
-    output.value = num1 + num2
+    let output = 0;
+    switch(op){
+        case '+': output = num1 + num2; break;
+        case '-': output = num1 - num2; break;
+        case '*': output = num1 * num2; break;
+        case '/': output = num1 / num2; break;
+    }
+
+    document.getElementById("output").value = output
 }
